a2b482a04424567cf4dc67e682b8d82059fce1be
yceline-yu <cellystuff5@gmail.com>
2021-04-08 14:16:21 -0700

    added makeBoard js, reworded file

--- connect4.js
+++ connect4.js
@@ -7,45 +7,61 @@
  * board fills (tie)
  */
 
-var WIDTH = 7;
-var HEIGHT = 6;
+const WIDTH = 7;
+const HEIGHT = 6;
 
-var currPlayer = 1; // active player: 1 or 2
-var board = []; // array of rows, each row is array of cells  (board[y][x])
+let currPlayer = 1; // active player: 1 or 2
+let board = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
  */
 
-function makeBoard() {
+//the js data for making the board
+function makeBoard(width,height) {
   // TODO: set "board" to empty HEIGHT x WIDTH matrix array
+  let rowArray = [];
+  let boardArray = [];
+  let i = 0;
+  let j = 0;
+  while (i < width){
+    rowArray.push(null);
+    i++
+  }
+  while (j < height){
+    boardArray.push(rowArray);
+    j++
+  }
+return boardArray;
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
 
+//the js for adding the board to the document
 function makeHtmlBoard() {
   // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
 
-  // TODO: add comment for this code
+  // TODO: add comment for this code, makes the table row element and assigns id, adds click function
   var top = document.createElement("tr");
   top.setAttribute("id", "column-top");
   top.addEventListener("click", handleClick);
 
-  // TODO: add comment for this code
-  for (var x = 0; x < WIDTH; x++) {
-    var headCell = document.createElement("td");
+  // TODO: add comment for this code, sets the table data id, the id starts from 0 ends with 7, adds to
+  //can id start with numbers? nope, will need to fix
+  for (let x = 0; x < WIDTH; x++) { 
+    let headCell = document.createElement("td");
     headCell.setAttribute("id", x);
-    top.append(headCell);
+    top.append(headCell); //adds complete table data with id to table row
   }
   htmlBoard.append(top);
 
   // dynamically creates the main part of html board
   // uses HEIGHT to create table rows
   // uses WIDTH to create table cells for each row
-  for (var y = 0; y < HEIGHT; y++) {
+  for (let y = 0; y < HEIGHT; y++) {
     // TODO: Create a table row element and assign to a "row" variable
 
-    for (var x = 0; x < WIDTH; x++) {
+    for (let x = 0; x < WIDTH; x++) {
       // TODO: Create a table cell element and assign to a "cell" variable
 
       // TODO: add an id, y-x, to the above table cell element
@@ -82,10 +98,10 @@ function endGame(msg) {
 
 function handleClick(evt) {
   // get x from ID of clicked cell
-  var x = +evt.target.id;
+  let x = +evt.target.id;
 
   // get next spot in column (if none, ignore click)
-  var y = findSpotForCol(x);
+  let y = findSpotForCol(x);
   if (y === null) {
     return;
   }
@@ -125,8 +141,8 @@ function checkForWin() {
   // using HEIGHT and WIDTH, generate "check list" of coordinates
   // for 4 cells (starting here) for each of the different
   // ways to win: horizontal, vertical, diagonalDR, diagonalDL
-  for (var y = 0; y < HEIGHT; y++) {
-    for (var x = 0; x < WIDTH; x++) {
+  for (let y = 0; y < HEIGHT; y++) {
+    for (let x = 0; x < WIDTH; x++) {
       // TODO: assign values to the below variables for each of the ways to win
       // horizontal has been assigned for you
       // each should be an array of 4 cell coordinates:
