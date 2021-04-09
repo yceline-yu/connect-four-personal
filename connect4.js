093e4ff9cf686bc9e22884626a8e469ba514f781
Matt Riese <rise.matt@gmail.com>
2021-04-08 17:00:19 -0700

    finished findspotforcol

--- connect4.js
+++ connect4.js
@@ -74,7 +74,12 @@ function makeHtmlBoard() {
 
 function findSpotForCol(x) {
   // TODO: write the real version of this, rather than always returning 0
-  return 0;
+  for(let y = HEIGHT-1; y >= 0; y--){
+    if(board[y][x] === null){
+      return y
+    }
+  }
+  return null;
 }
 
 /** placeInTable: update DOM to place piece into HTML table of board */
@@ -109,7 +114,7 @@ function handleClick(evt) {
   }
 
   // place piece in board and add to HTML table
-  // TODO: add line to update in-memory board
+  // add line to update in-memory board
   placeInTable(y, x);
   board[y][x] = currPlayer;
 
@@ -119,17 +124,15 @@ function handleClick(evt) {
   }
 
   // check for tie
-  // TODO: check if all cells in board are filled; if so call, call endGame
-  let tieCheck = board.every(val, function(val){
-    return board[val] !== null;
-  })
-  
+  // check if all cells in board are filled; if so call, call endGame
+  let tieCheck = board.flat().every(elem => !elem);
+
   if (tieCheck){
     endGame();
   }
 
   // switch players
-  // TODO: switch currPlayer 1 <-> 2
+  // switch currPlayer 1 <-> 2
   if (currPlayer === 1){
     currPlayer = 2;
   } else {
