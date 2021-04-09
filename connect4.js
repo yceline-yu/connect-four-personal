836534fd3bdd00b36a77b7d6c9349838dae85851
yceline-yu <cellystuff5@gmail.com>
2021-04-08 18:08:19 -0700

    win function, board fix

--- connect4.js
+++ connect4.js
@@ -20,15 +20,14 @@ let board = []; // array of rows, each row is array of cells  (board[y][x])
 //the js data for making the board
 //set "board" to empty HEIGHT x WIDTH matrix array
 function makeBoard() {
-  let boardArray = [];
   for(let y = 0; y < HEIGHT; y++){
     let rowArray = [];
     for(let x = 0; x < WIDTH; x++){
       rowArray.push(null);
     }
-    boardArray.push(rowArray)
+    board.push(rowArray)
   }
-return boardArray;
+return board;
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
@@ -89,7 +88,7 @@ function placeInTable(y, x) {
   console.log("placeInTable runs")
   let piece = document.createElement("div");
   piece.classList.add("piece");
-  piece.classList.add("p1");
+  piece.classList.add(`p${currPlayer}`);
   let cell = document.getElementById(`${y}-${x}`);
   console.log(cell);
   cell.append(piece);
@@ -147,6 +146,7 @@ function checkForWin() {
 
   /** _win:
    * takes input array of 4 cell coordinates [ [y, x], [y, x], [y, x], [y, x] ]
+   * y = height, x = width
    * returns true if all are legal coordinates for a cell & all cells match
    * currPlayer
    */
@@ -154,7 +154,22 @@ function checkForWin() {
 
     // TODO: Check four cells to see if they're all legal & all color of current
     // player
+    //nest loop -> what am i trying to get? -> coordiante values
+    //what will i do with values -> see if it gives a value from board array
+    //if value given i need to check color of it to see if all coordinates are the same
 
+cells.every(cell => {
+  let y = cell[0];
+  let x = cell[1];
+  return y >= 0 && y < HEIGHT && x >= 0 && x < WIDTH && board[y][x] === currPlayer;
+}
+)
+  //   for (let y = 0; y < cells.length; y++){
+  //     for (let x = 0; x < cells[y].length, x++){
+  //       if (board[cell[y][x]][cell[x][y]] === currPlayer){
+  //       }
+  //     }
+  //   }
   }
 
   // using HEIGHT and WIDTH, generate "check list" of coordinates
@@ -162,15 +177,15 @@ function checkForWin() {
   // ways to win: horizontal, vertical, diagonalDR, diagonalDL
   for (let y = 0; y < HEIGHT; y++) {
     for (let x = 0; x < WIDTH; x++) {
-      // TODO: assign values to the below variables for each of the ways to win
+      // assign values to the below variables for each of the ways to win
       // horizontal has been assigned for you
       // each should be an array of 4 cell coordinates:
       // [ [y, x], [y, x], [y, x], [y, x] ]
 
       let horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
-      let vert;
-      let diagDL;
-      let diagDR;
+      let vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
+      let diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
+      let diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
 
       // find winner (only checking each win-possibility as needed)
       if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
